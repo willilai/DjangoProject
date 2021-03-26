ff9e9e58e38483122df91052e058c611e196d751
willilai <william.lai23@bishops.com>
2021-03-26 11:38:04 -0700

    Added an admin superuser, added more fields to the Specie model

--- mysite/WhaleProfiles/models.py
+++ mysite/WhaleProfiles/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Specie(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=500)
     numWhales = models.IntegerField(default=0)
+    diet = models.CharField(max_length=500)
+    size = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
     def __str__(self):
         return self.name
