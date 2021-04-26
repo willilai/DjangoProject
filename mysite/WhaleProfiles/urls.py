10448d7fd92be1b9cc9902de3b69596351c76482
willilai <william.lai23@bishops.com>
2021-04-26 10:41:29 -0700

    Added links to make visiting each template easier

--- mysite/WhaleProfiles/urls.py
+++ mysite/WhaleProfiles/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'WhaleProfiles'
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
+    path('index/', views.Index.as_view(), name='index'),
     path('addSpecie/', views.AddSpecie.as_view(), name='addSpecie'),
     path('editSpecie/', views.EditSpecie.as_view(), name='editSpecie'),
 ]
